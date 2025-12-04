8dc246470b6735ca26162b7201d1eaffe9cab2ad
M4k1Sr <shimurushimuru81@gmail.com>
2025-12-04 09:42:05 +0900

    Add files via upload

---
+++ Src/Object/Actor/Stage.cpp
@@ -0,0 +1,81 @@
+#include <DxLib.h>
+#include "../../Utility/AsoUtility.h"
+#include "../../Object/Common/Transform.h"
+#include "../../Object/Collider/ColliderModel.h"
+#include "../../Manager/InputManager.h"
+#include "../../Manager/SceneManager.h"
+#include "../../Manager/ResourceManager.h"
+#include "../../Manager/Resource.h"
+#include "../../Application.h"
+#include "Stage.h"
+
+Stage::Stage(void)
+	:
+	ActorBase()
+{
+}
+
+Stage::~Stage(void)
+{
+}
+
+void Stage::Update(void)
+{
+
+	transform_.Update();
+}
+
+void Stage::Draw(void)
+{
+	// モデル描画
+	MV1DrawModel(transform_.modelId);
+
+}
+
+void Stage::Release(void)
+{
+}
+
+void Stage::InitLoad(void)
+{
+	// モデル読み込み
+	transform_.SetModel(resMng_.LoadModelDuplicate(
+		ResourceManager::SRC::MAIN_STAGE));
+
+}
+
+void Stage::InitTransform(void)
+{
+	transform_.scl = AsoUtility::VECTOR_ONE;
+	transform_.quaRot = Quaternion::Identity();
+	transform_.quaRotLocal = Quaternion::Identity();
+	transform_.pos = MAIN_STAGE_POS;
+	transform_.Update();
+
+}
+
+void Stage::InitCollider(void)
+{
+	// DxLib側の衝突情報セットアップ
+	MV1SetupCollInfo(transform_.modelId);
+
+	// モデルのコライダ
+	ColliderModel * colModel =
+		new ColliderModel(ColliderBase::TAG::STAGE, &transform_);
+
+	// 除外フレーム設定
+	for (const std::string& name : EXCLUDE_FRAME_NAMES)
+	{
+		colModel->AddExcludeFrameIds(name);
+	}
+
+	ownColliders_.emplace(static_cast<int>(COLLIDER_TYPE::MODEL), colModel);
+}
+
+void Stage::InitAnimation(void)
+{
+}
+
+void Stage::InitPost(void)
+{
+}
---
+++ Src/Object/Collider/ColliderModel.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "ColliderBase.h"
+
+class ColliderModel : public ColliderBase
+{
+public:
+
+	// コンストラクタ
+	ColliderModel(TAG tag, const Transform* follow);
+
+	// デストラクタ
+	~ColliderModel(void) override;
+
+	// 指定された文字を含むフレームを衝突判定から除外
+	void AddExcludeFrameIds(const std::string& name);
+
+	// 衝突判定から除外するフレームをクリアする
+	void ClearExcludeFrame(void);
+
+	// 除外フレーム判定
+	bool IsExcludeFrame(int frameIdx) const;
+
+protected:
+
+	// 衝突判定から除外するフレーム番号
+	std::vector<int> excludeFrameIds_;
+
+	// デバッグ用描画
+	void DrawDebug(int color) override {};
+
+};
